9863a044bde48b0c9c98f00a290aa481c13aaa5b
Adebisola <adeniyiadebisola@gmail.com>
2022-12-05 06:23:24 -0800

    string manipulation

---
+++ repeat_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+string = "Ha"
+new_string = 5 * string
+print(new_string)
+
---
+++ string_type.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+string = "word"
+print(string[-1])
+print(string[-2])
+print(string[-3])
+print(string[-4])
+
+print(string[1:3])
+print(string[2:])
+print(string[:3])
+
+first = "First"
+second = "Second"
+
+new_string = first + " " + second
+print(new_string)
