eb4df5dd73b3faf32926fba2935fc164de3d4946
Adebisola <adeniyiadebisola@gmail.com>
2022-12-17 10:20:01 -0800

    more practice

---
+++ format_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+age = 30
+
+name = "John"
+txt = "I am {} and i am {} years old"
+print(txt.format(name,age))
+
+quantity = 300
+itemno = 67
+price = 49.99
+myOrder = "I want to buy {} doughnuts, the itemno is {} and I am paying {} dollars"
+print(myOrder.format(quantity,itemno,price))
