5d806b5bb9ec8ad704d59aea0b1c9c60668bb493
Adebisola <adeniyiadebisola@gmail.com>
2022-12-17 09:55:35 -0800

    practicing some python methods

---
+++ mod_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+a = "Hello World"
+print(a.upper())
+b = "Football is life!"
+print(b.lower())
+c = " Python is so cool! "
+print(c.strip())
+x = "Jello"
+print(x.replace("J","H"))
+
+y = "Life, it's so beautiful,funny and complex"
+print(y.split(","))
