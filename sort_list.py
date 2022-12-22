d3ff4028ec946576b6aa4422015a7c05e4725e27
Adebisola <adeniyiadebisola@gmail.com>
2022-12-22 12:06:11 -0800

    tuple pratice

--- sort_list.py
+++ sort_list.py
@@ -4,3 +4,21 @@ sort_list.sort()
 print(sort_list)
 sort_list.sort(reverse = True)
 print(sort_list)
+
+
+def myfunc(n):
+    return abs(n-50)
+
+a_list = [100,16,55,23,90,45]
+""" this sorts the list based on how close
+the number is t0 50
+"""
+a_list.sort(key = myfunc)
+print(a_list)
+
+cars = ["Volvo", "benz", "tesla", "Volkswagen", "Ford","mazda"]
+cars.sort(key = str.lower)
+print(cars)
+
+cars.reverse()
+print(cars)
---
+++ tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a_tuple = (23,14,19,60)
+print(type(a_tuple))
+b_tuple = tuple((True,False,True))
+print(b_tuple)
