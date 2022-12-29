91ecf1447bbf1568f4e088fd083766960e3105bd
Adebisola <adeniyiadebisola@gmail.com>
2022-12-29 06:03:05 -0800

    dict

---
+++ dict.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+a_dict = {
+        "animal": "shark",
+        "habitat": "sea",
+        "name": "betty",
+        "age": 12
+        }
+print(a_dict)
+print(a_dict["animal"])
+print(len(a_dict))
+print(type(a_dict))
+this_dict = dict(name = "Bunmi", age = 20, country = "Nigeria")
+print(this_dict)
