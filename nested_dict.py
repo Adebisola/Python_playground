ebf786daffdb77e7b6e3beb2f1325cd2410db9e3
Adebisola <adeniyiadebisola@gmail.com>
2022-12-30 12:15:28 -0800

    nested dicts

---
+++ nested_dict.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+child1 = {
+        "name" : "Tolu",
+        "age" : 24
+        }
+child2 = {
+        "name" : "Matt",
+        "age" : 20
+        }
+child3 = {
+        "name" : "Bimpe",
+        "age" : 16
+        }
+
+myFamily = {
+        "child1" : child1,
+        "child2" : child2,
+        "child3" : child3
+        }
+print(myFamily)
