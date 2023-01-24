aa0ece98722a82eb882e889bb9554bbc445955f2
Adebisola <adeniyiadebisola@gmail.com>
2023-01-24 11:19:08 -0800

    inheritance

--- inherit.py
+++ inherit.py
@@ -12,7 +12,17 @@ myName = Person("Sola", "Akinsote")
 myName.printname()
 
 class Person1(Person):
-    pass
+    def __init__(self, firstname, lastname, year):
+        super().__init__(firstname, lastname)
+        self.age = 34
+        self.graduationyear = year
+    
+    def welcome(self):
+        print("Hello",self.firstname,self.lastname,"Welcome to the class of",self.graduationyear) 
 
-Student = Person1("Bolanle", "Ajadi")
+
+Student = Person1("Bolanle", "Ajani", 2022)
 Student.printname()
+print(Student.graduationyear)
+print(Student.age)
+Student.welcome()
