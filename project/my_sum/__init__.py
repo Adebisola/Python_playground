499cf3bf5792b9acdf8e74698537f215def46f91
Adebisola <adeniyiadebisola@gmail.com>
2023-02-11 12:48:47 -0800

    write a simple test in python

---
+++ project/my_sum/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# defines a function that adds a list/tuple
+
+def sum(arg):
+    # represents a function that add the values in a list/tuple
+
+    total = 0
+    for val in arg:
+        total += val
+    return total
